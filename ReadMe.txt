76efc9e074b0731885771e15fe453f98d10afc69
Martin O'Connor <moconnor@computing.dcu.ie>
2012-05-08 01:13:20 +0100

    First Commit

---
+++ ReadMe.txt
@@ -0,0 +1,76 @@
+--------
+Name: 		TweakFox.xpi
+Version: 	1.3
+Date: 		07-May-2012
+Author: 	Martin Francis O'Connor
+HomePage:	http://www.computing.dcu.ie/~moconnor
+
+--------
+
+What is TweakFox?
+-----------------
+This project contains the source code for my first Firefox Browser addon or
+extension called TweakFox.
+
+
+What does TweakFox do?
+----------------------
+The purpose of the Tweakfox Firefox extension is twofold:
+
+1) To automatically customise the navigation bar in Firefox according to a 
+   user specified custom arrangement. 
+   
+2) To automatically configure virtually any Firefox preferences according 
+   to a given set of user-specified preferences.
+   
+   Note:  Firefox preferences may be accessed and modified at any time 
+   by entering "about:config" into the URL bar.
+   
+
+What is the motivation behind TweakFox?
+---------------------------------------
+For stability, performance and security reasons, It is recommended to use the 
+latest stable release of the Firefox browser.
+
+However, there is a significant time investment required each time to manually 
+configure the browser according to my preferences.  More specifically I have
+to manually:
+1) configure all of the Firefox options according to my needs and preferences.
+2) customise the navigation bar according to my preferences.
+3) Individually download and install several addons that I find essential 
+   such as: quickdrag, adblock plus; close tab by double click; 
+   all-in-one sidebar, site launcher and so on.
+4) Individually configure each addon according to my preferences.
+
+Tweakfox automates completely steps 1 and 2 above.  I subsequently created a 
+new container addon which I call firefoxKeyAddons.xpi containing all of my 
+favourite addons preconfigured according to my preferences plus my Tweakfox 
+addon.  Consequently, I can take a clean installation of a new stable release
+of the Firefox browser and by installing just one addon - namely 
+firefoxKeyAddons.xpi, the Firefox browser is instantly configured with all
+of my favourite addons, customisations and preferences with no extra work
+required.
+
+
+How to adapt and modify TweakFox!
+---------------------------------
+There are only two javascript files in TweakFox:
+  TweakFox/chromecontent/tweakfox.js
+  TweakFox/defaults/preferences/tweakfoxprefs.js
+  
+tweakfox.js contains all of the functionality of TweakFox.
+The code is well documented and quite easy to follow. This script is only run 
+once, when the addon is initially installed.  By running the script only once, 
+any future customisations made by the user will be saved and not overwritten.
+
+tweakfoxprefs.js contains the user specified preferences.  You may modify 
+these preferences or enter new ones according to your needs.
+
+TweakFox adds six new custom preferences:  They are:
+  pref("extensions.tweakfox.firstRun", true);
+  pref("extensions.tweakfox.enableDownloadPreferences", true);
+  pref("extensions.tweakfox.downloadFolder", "c:\\downloads");
+  pref("extensions.tweakfox.enableCustomNavBar", true);
+  pref("extensions.tweakfox.customNavBar", "bookmarks-menu-button-container,sync-button,separator,unified-back-forward-button,separator,home-button,separator,reload-button,stop-button,navigator-throbber,urlbar-container,search-container,fullscreenflex,window-controls");
+  pref("extensions.tweakfox.cleanHistoryOnExit", true);
+  
---
+++ TweakFox/chrome/content/tweakfox.js
@@ -0,0 +1,125 @@
+/*
+Name:   	tweakfox.js
+Version: 	1.3
+Author:  	Martin Francis O'Connor
+Date: 		07-May-2012
+
+Description:
+
+The purpose of this Firefox extension is to customise a vanilla installation
+of the Firefox browser (or FirefoxPortable) according to a user-specified 
+set of preferences.  
+
+A new version of Firefox is released on a frequent basis.  For security, 
+performance and stability reasons, it is preferable to use the latest stable
+release of the Firefox browser. 
+
+This results in a cycle of duplicated effort every time to tweak the many 
+Firefox preferences, customise the Firefox navigation bar and install a set
+of favourite addons so as to have an efficent and effective browser.
+
+TweakFox performs the specified tweaks and customisations and consequently,
+saves a lot of time, while guaranteeing the user has a browser customised 
+and tweaked according to their preferences.
+
+*/
+
+ var tweakfox = {
+ 	// The Init Function is run only once - when the addon is first installed.
+	Init : function()
+	{	
+		// Access the Firefox Preferences Service
+		tweakfox.prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+		// Retrive the branch of preferences for the tweakfox extension.
+		tweakfox.prefs = tweakfox.prefs.getBranch("extensions.tweakfox.");
+		// Determine if tweakfox has just been installed.
+		var isFirstRun = tweakfox.prefs.getBoolPref("firstRun");
+		
+		if (isFirstRun == true)
+		{
+			// Retrieve the remaining preferences.
+			var enableDownloadPrefs = tweakfox.prefs.getBoolPref("enableDownloadPreferences");
+			var enableCustomNavBar = tweakfox.prefs.getBoolPref("enableCustomNavBar");
+			var cleanHistoryOnExit = tweakfox.prefs.getBoolPref("cleanHistoryOnExit");
+			
+			// Check if the browser download preferences should be set.
+			if (enableDownloadPrefs == true) {
+				// Obtain the user-specified download folder.
+				var downloadFolder = tweakfox.prefs.getCharPref("downloadFolder");
+				// Set the browser download preferences
+				tweakfox.setBrowserDownLoadPreferences(downloadFolder);			
+			}
+
+			// Check if the brower navigation bar should be customised.
+			if (enableCustomNavBar == true) {
+				// Obtain the user specified custom navigation bar arrangement
+				var customNavBar = tweakfox.prefs.getCharPref("customNavBar");
+				// Customise the Firefox Navigation bar.
+				tweakfox.customiseFirefoxNavBar(customNavBar);		
+			}			
+
+			// Check if the brower history should be cleaned when closing browser.
+			if (cleanHistoryOnExit == true) {
+				tweakfox.cleanHistoryOnExit();	
+			}
+			
+			// New versions of Firefox may introduce new UI features.
+			// Consequently, the user may want to further customise the
+			// settings.  Therefore, disable this script from running 
+			// again so as to ensure any new customisations are not 
+			// lost or overwritten.
+ 			tweakfox.prefs.setBoolPref("firstRun", false);
+			
+		} 
+	},
+	
+	//Method to Set the browser download preferences
+	setBrowserDownLoadPreferences : function(downloadFolder) 
+	{		
+		// Access the browser preferences.
+		var browserPrefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components. interfaces.nsIPrefBranch);
+				
+		// Configure the download preferences	
+		browserPrefs.setCharPref('browser.download.dir', downloadFolder);
+		browserPrefs.setCharPref('browser.download.defaultFolder', downloadFolder);
+		browserPrefs.setCharPref('browser.download.downloadDir', downloadFolder);			
+		browserPrefs.setBoolPref('browser.download.useDownloadDir',true);
+		browserPrefs.setCharPref('browser.download.lastDir', downloadFolder);
+		// 2=Use the last folder specified for a download; 1=default folder; 0=desktop
+		browserPrefs.setIntPref('browser.download.folderList', 2);
+		browserPrefs.setBoolPref('browser.download.manager.showWhenStarting',false);
+		// 1=Remove entries from download manager when exiting browser;
+		// 0=Upon successful download; 2=Manually (default)
+		browserPrefs.setIntPref('browser.download.manager.retention', 1);
+	},
+
+	
+	//Method to customise the browser navigation bar
+	customiseFirefoxNavBar : function(navBar) 
+	{
+		// Customise the Firefox navigation bar according to my preferences
+		var firefoxnavbar = document.getElementById("nav-bar");
+		var currentset = firefoxnavbar.currentSet;
+		currentset = navBar;			
+		firefoxnavbar.setAttribute("currentset",currentset);
+		firefoxnavbar.currentSet = currentset;
+		// Finally save the navigation bar customisation. 
+		// (As of Firefox version 12, the navigation bar customisations are
+		// stored in the file localstore.rdf located in the Firefox user 
+		// profile folder).
+		document.persist("nav-bar","currentset");
+	},	
+
+
+	//Method to customise the browser navigation bar
+	cleanHistoryOnExit : function() 
+	{
+		// Access the browser preferences.
+		var browserPrefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components. interfaces.nsIPrefBranch);
+		browserPrefs.setBoolPref('privacy.sanitize.sanitizeOnShutdown',true);
+		browserPrefs.setBoolPref('places.history.enabled',false);
+	},
+}	
+
+// Run the Init Function as soon as the browser loads.
+window.addEventListener("load", tweakfox.Init, false);
